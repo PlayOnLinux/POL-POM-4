920481cfa9bef6d8582b1d24d9b0a0fb5004d0a2
Quentin Pâris <github@playonlinux.com>
2011-08-08 00:00:19 +0200

    Pleins de trucs

--- python/wine_versions.py
+++ python/wine_versions.py
@@ -23,6 +23,12 @@ import wx, wx.animate
 
 import lib.Variables as Variables
 import lib.lng, threading
+
+if(os.environ["POL_OS"] == "Mac"):
+	os_pref = "darwin"
+else:
+	os_pref = "linux"
+	
 lib.lng.Lang()
 timeout = 2
 #socket.setdefaulttimeout(timeout)
@@ -104,10 +110,11 @@ def keynat(string):
     return r
 
 class getVersions(threading.Thread):
-	  def __init__(self):
+	  def __init__(self, arch="x86"):
 		threading.Thread.__init__(self)
 		self.thread_message = "#WAIT#"
 		self.versions = []
+		self.architecture = arch
 		self.start()
 
 	  def download(self, game):
@@ -117,12 +124,12 @@ class getVersions(threading.Thread):
 		self.thread_running = True
 		while(self.thread_running):
 			if(self.thread_message == "get"):
+				wfolder = os_pref+"-"+self.architecture
 				try :
 					socket.setdefaulttimeout(timeout)
-					if(os.environ["POL_OS"] == "Mac"):
-						url = "http://wine.playonlinux.com/darwin-i386/LIST"
-					else :
-						url = "http://wine.playonlinux.com/linux-i386/LIST"
+
+
+					url = os.environ["WINE_SITE"]+"/"+wfolder+".lst"
 					
 					#print(url)
 					req = urllib2.Request(url)
@@ -131,6 +138,7 @@ class getVersions(threading.Thread):
 					socket.setdefaulttimeout(None)
 					available_versions = handle.read()
 					available_versions = string.split(available_versions,"\n")
+					
 					self.i = 0
 					self.versions_ = []
 					while(self.i < len(available_versions) - 1):
@@ -138,7 +146,7 @@ class getVersions(threading.Thread):
 						version = informations[1]
 						package = informations[0]
 						sha1sum = informations[2]
-						if(not os.path.exists(Variables.playonlinux_rep+"/WineVersions/"+version)):
+						if(not os.path.exists(Variables.playonlinux_rep+"/wine/"+wfolder+"/"+version)):
 							self.versions_.append(version)
 						self.i += 1	
 					self.versions_.reverse()
@@ -158,73 +166,55 @@ class Onglets(wx.Notebook):
 			self.notebook = wx.Notebook.__init__(self, parent, -1)
 			self.images_onglets = wx.ImageList(16, 16)
 			self.images_onglets.Add(wx.Bitmap(Variables.playonlinux_env+"/etc/onglet/wine.png"))
-			self.images_onglets.Add(wx.Bitmap(Variables.playonlinux_env+"/etc/onglet/input-gaming.png"))
 			self.SetImageList(self.images_onglets)
-
-		def liste_versions(self):
-		    self.panelFenp = wx.Panel(self, -1)
-		    self.imagesapps = wx.ImageList(22, 22)
-		    self.imagesapps_i = wx.ImageList(22, 22)
-
-		    # self.searchbox = wx.TextCtrl(self.panelFenp, 110, size=(464,25), pos=(319,9))
-		    # self.searchcaption = wx.StaticText(self.panelFenp, -1, Lng.search, (220,15), wx.DefaultSize)
-		    self.list_apps = wx.TreeCtrl(self.panelFenp, 106, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|Variables.widget_borders, size=(550, 212), pos=(10,30))
-		    self.list_apps.SetImageList(self.imagesapps)
-		    self.list_apps.SetSpacing(0);
-
-	 	    self.new_panel = wx.Panel(self.panelFenp, -1, pos=(10,505), size=(100,100))
-	   	    #self.animation = wx.animate.GIFAnimationCtrl(self.new_panel, -1, Variables.playonlinux_env+"/etc/24-0.gif", (0,0))
-		    #self.animation.Play()
-
-		    self.list_ver_installed = wx.TreeCtrl(self.panelFenp, 107, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|Variables.widget_borders, size=(550, 212), pos=(10,281))
-		    self.list_ver_installed.SetImageList(self.imagesapps_i)
-		    self.list_ver_installed.SetSpacing(0);
-		    wx.StaticText(self.panelFenp, -1, _("Installed Wine versions: "),(10,261))
-		    wx.StaticText(self.panelFenp, -1, _("Available Wine versions: "),(10,10))
-		    # self.content =  wx.TextCtrl(self.panelFenp, 107, pos=(10,301), size=(562,212), style = wx.TE_MULTILINE | wx.TE_RICH2 | wx.CB_READONLY | wx.RAISED_BORDER)
-
-		    self.button_rm = wx.Button(self.panelFenp, wx.ID_REMOVE, _("Remove"), pos=(380, 500), size=wx.DefaultSize)
-		    self.button_in = wx.Button(self.panelFenp, wx.ID_ADD, _("Add"), pos=(480, 500), size=wx.DefaultSize)
-
-		    self.button_rm.Enable(False)
-		    self.button_in.Enable(False)
-		    self.AddPage(self.panelFenp, _("Wine versions"), imageId=0)
+			self.panelFenp = {} 
+			self.imagesapps = {}
+			self.imagesapps_i = {} 
+			self.list_apps = {}
+			self.new_panel = {} 
+			self.list_ver_installed = {}
+			self.button_rm = {}
+			self.button_in = {} 
+			
+		def liste_versions(self, arch="x86"):
+			if(arch == "amd64"):
+				add=100
+			else:
+				add=0
+			self.panelFenp[arch] = wx.Panel(self, -1)
+			self.imagesapps[arch] = wx.ImageList(22, 22)
+			self.imagesapps_i[arch] = wx.ImageList(22, 22)
 
 
+			self.list_apps[arch] = wx.TreeCtrl(self.panelFenp[arch], 106+add, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|Variables.widget_borders, size=(550, 212), pos=(10,30))
+			self.list_apps[arch].SetImageList(self.imagesapps[arch])
+			self.list_apps[arch].SetSpacing(0);
 
-		def VersionsLoad(self):
-		    dir_v = []
-		    files = os.listdir(Variables.playonlinux_rep+"/WineVersions/")
-		    self.i = 0
-		    while(self.i < len(files)):
-			if(os.path.isdir(Variables.playonlinux_rep+"/WineVersions/"+files[self.i])):
-				dir_v.append(files[self.i])
-			self.i += 1	
+			self.new_panel[arch] = wx.Panel(self.panelFenp[arch], -1, pos=(10,505), size=(100,100))
 
-		    dir_v.sort()
-		    dir_v.reverse()
-		    dir_v.insert(0,"System")
+			self.list_ver_installed[arch] = wx.TreeCtrl(self.panelFenp[arch], 107+add, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|Variables.widget_borders, size=(550, 212), pos=(10,281))
+			self.list_ver_installed[arch].SetImageList(self.imagesapps_i[arch])
+			self.list_ver_installed[arch].SetSpacing(0);
+			wx.StaticText(self.panelFenp[arch], -1, _("Installed Wine versions: "),(10,261))
+			wx.StaticText(self.panelFenp[arch], -1, _("Available Wine versions: "),(10,10))
 
-		    self.MenuVer.Destroy()
-		    self.MenuVer = wx.ComboBox(self.panelApps, 112, value=dir_v[0], pos=wx.Point(10, 460),size=wx.DefaultSize, choices=dir_v)
+			self.button_rm[arch] = wx.Button(self.panelFenp[arch], 108+add, _("Remove"), pos=(380, 500), size=wx.DefaultSize)
+			self.button_in[arch] = wx.Button(self.panelFenp[arch], 109+add,_("Install"), pos=(480, 500), size=wx.DefaultSize)
 
-	        def liste_games(self):
-	            self.panelApps = wx.Panel(self, -1)
+			self.button_rm[arch].Enable(False)
+			self.button_in[arch].Enable(False)
+			self.AddPage(self.panelFenp[arch], _("Wine versions")+" ("+arch+")", imageId=0)
 
-	 	    self.images = wx.ImageList(22, 22)
-		    wx.StaticText(self.panelApps, -1, _("My Applications")+" :",(10,10))
-		    self.list_game = wx.TreeCtrl(self.panelApps, 111, style=wx.TR_HIDE_ROOT|wx.TR_FULL_ROW_HIGHLIGHT|Variables.widget_borders, size=(550, 400), pos=(10,30))
-		    self.list_game.SetImageList(self.images)
-		    self.list_game.SetSpacing(0);
-		    self.version = wx.StaticText(self.panelApps, -1, _("Version:"),(10,440))
 
-		    self.MenuVer = wx.ComboBox(self.panelApps, 112)
-		    self.VersionsLoad()
-		    self.AddPage(self.panelApps, _("My Applications"), imageId=1)
+
+
+
 
 class MainWindow(wx.Frame):
 	  def __init__(self,parent,id,title):
-	    self.download = getVersions()
+	    self.download32 = getVersions("x86")
+	    self.download64 = getVersions("amd64")
+	    
 	    wx.Frame.__init__(self, parent, -1, title, size = (592, 588+Variables.windows_add_size), style = wx.CLOSE_BOX | wx.CAPTION | wx.MINIMIZE_BOX)
 	    self.timer = wx.Timer(self, 1)
 	    self.SetIcon(wx.Icon(Variables.playonlinux_env+"/etc/playonlinux.png", wx.BITMAP_TYPE_ANY))
@@ -234,85 +224,73 @@ class MainWindow(wx.Frame):
 	    #self.sizer = wx.BoxSizer(wx.VERTICAL)   
 	    #self.sizer.Add(self.onglets, 15, wx.EXPAND|wx.ALL, 2)
 
-	    self.getVersions()
+	    self.getVersions("x86")
+	    self.getVersions("amd64")	
 	    #self.panel.SetSizer(self.sizer)
 	    #self.panel.SetAutoLayout(True)
 
 	    self.onglets.liste_versions()
-	    self.onglets.liste_games()
+	    self.onglets.liste_versions("amd64")
+	    self.oldreload32=""
+	    self.oldreload64=""
+	    
+	    self.oldversions32 = []
+	    self.oldversions64 = []
 
-	    self.oldreload=""
-	    self.oldversions = []
-
-	    self.add_games()
 	    #self.button = wx.Button(self.panels_buttons, wx.ID_CLOSE, _("Close"), pos=(510, 5), size=wx.DefaultSize)
 
 	    wx.EVT_BUTTON(self, wx.ID_CLOSE, self.closeapp)
 	    wx.EVT_CLOSE(self, self.closeapp)
-	    wx.EVT_BUTTON(self, wx.ID_REMOVE, self.delete)
-	    wx.EVT_BUTTON(self, wx.ID_ADD, self.install)
-	    wx.EVT_TREE_SEL_CHANGED(self, 106, self.unselect)
-	    wx.EVT_TREE_SEL_CHANGED(self, 107, self.unselect)
-	    wx.EVT_TREE_SEL_CHANGED(self, 111, self.select_game)
-	    wx.EVT_COMBOBOX(self, 112, self.assign)
+	    wx.EVT_TREE_SEL_CHANGED(self, 106, self.unselect32)
+	    wx.EVT_TREE_SEL_CHANGED(self, 107, self.unselect32)
+	    wx.EVT_BUTTON(self, 108, self.delete32)
+	    wx.EVT_BUTTON(self, 109, self.install32)
+
+	    wx.EVT_TREE_SEL_CHANGED(self, 206, self.unselect64)
+	    wx.EVT_TREE_SEL_CHANGED(self, 207, self.unselect64)
+	    wx.EVT_BUTTON(self, 208, self.delete64)
+	    wx.EVT_BUTTON(self, 209, self.install64)
+	
 	    self.Bind(wx.EVT_TIMER, self.AutoReload, self.timer)
 	    self.timer.Start(200)
 
 	  def AutoReload(self, event):
-	    reload = os.listdir(Variables.playonlinux_rep+"/WineVersions")
-	    if(self.download.thread_message == "Ok" or self.download.thread_message == "Err"):
-		self.onglets.new_panel.Hide()
-		self.WriteVersions()
-		self.download.thread_message = "Wait"
-	    else:
-		if(self.download.thread_message != "Wait"):
-			self.onglets.new_panel.Show()
+		reload32 = os.listdir(Variables.playonlinux_rep+"/wine/"+os_pref+"-x86")
+		reload64 = os.listdir(Variables.playonlinux_rep+"/wine/"+os_pref+"-amd64")
+		if(self.download32.thread_message == "Ok" or self.download32.thread_message == "Err"):
+			self.onglets.new_panel["x86"].Hide()
+			self.WriteVersion()
+			self.download32.thread_message = "Wait"
 
-	    if(reload != self.oldreload):
-		self.getVersions()
-		self.onglets.VersionsLoad()
-		self.oldreload = reload
+		else:
+			if(self.download32.thread_message != "Wait"):
+				self.onglets.new_panel["x86"].Show()
+				
+		
+		if(self.download64.thread_message == "Ok" or self.download64.thread_message == "Err"):
+			self.onglets.new_panel["amd64"].Hide()
+			self.WriteVersion("amd64")
+			self.download64.thread_message = "Wait"
 
-	    if(self.download.versions != self.oldversions):
-		self.onglets.VersionsLoad()
-		self.oldversions = self.download.versions[:]
+		else:
+			if(self.download64.thread_message != "Wait"):
+				self.onglets.new_panel["amd64"].Show()
+				
 
-	  def assign(self, event):
-		game = self.onglets.list_game.GetItemText(self.onglets.list_game.GetSelection()).encode('utf-8')
-		version = self.onglets.MenuVer.GetValue()
-		SetWineVersion(game, version)
+		if(reload64 != self.oldreload64):
+			self.getVersions("amd64")
+			self.oldreload64 = reload64
 
+		if(reload32 != self.oldreload32):
+			self.getVersions()
+			self.oldreload32 = reload32
+			
+		if(self.download32.versions != self.oldversions32):
+			self.oldversions32 = self.download32.versions[:]
 
-	  def select_game(self, event):
-		self.onglets.MenuVer.Enable(True)
-		game = self.onglets.list_game.GetItemText(self.onglets.list_game.GetSelection()).encode('utf-8')
-		self.onglets.MenuVer.SetValue(GetWineVersion(game))		
+		if(self.download64.versions != self.oldversions64):
+			self.oldversions64 = self.download64.versions[:]
 
-
-	  def add_games(self):
-		self.onglets.MenuVer.Enable(False)
-		self.games = os.listdir(Variables.playonlinux_rep+"configurations/installed/")
-		self.games.sort()
-		self.onglets.images.RemoveAll()
-		self.onglets.list_game.DeleteAllItems()
-		root = self.onglets.list_game.AddRoot("")
-		self.i = 0
-		for game in self.games: 
-			self.file = Variables.playonlinux_rep+"configurations/installed/"+game
-			if(not os.path.isdir(self.file)):
-				fichier = open(self.file,"r").read()
-
-				if("wine " in fichier):
-					if(os.path.exists(Variables.playonlinux_rep+"/icones/32/"+game)):
-						file_icone = Variables.playonlinux_rep+"/icones/32/"+game
-					else:
-						file_icone = Variables.playonlinux_env+"/etc/playonlinux32.png"
-					bitmap = wx.Image(file_icone)
-					bitmap.Rescale(22,22,wx.IMAGE_QUALITY_HIGH)
-					bitmap = bitmap.ConvertToBitmap()
-					self.onglets.images.Add(bitmap)
-					self.onglets.list_game.AppendItem(root, game, self.i)
-					self.i = self.i+1
 
 
 	  def sizedirectory(self, path): 
@@ -322,71 +300,101 @@ class MainWindow(wx.Frame):
 	            size += os.path.getsize(os.path.join(root, fic))
 	    return size
 
-	  def unselect(self, event):
+	  def unselect32(self, event):
 		if(event.GetId() == 106):
-			self.onglets.list_ver_installed.UnselectAll()
-			self.onglets.button_rm.Enable(False)
-			self.onglets.button_in.Enable(True)
+			self.onglets.list_ver_installed["x86"].UnselectAll()
+			self.onglets.button_rm["x86"].Enable(False)
+			self.onglets.button_in["x86"].Enable(True)
 		if(event.GetId() == 107):
-			self.onglets.list_apps.UnselectAll()
-			self.onglets.button_rm.Enable(True)
-			self.onglets.button_in.Enable(False)
+			self.onglets.list_apps["x86"].UnselectAll()		
+			self.onglets.button_rm["x86"].Enable(True)
+			self.onglets.button_in["x86"].Enable(False)
 
-	  def delete(self, event):
-		version = self.onglets.list_ver_installed.GetItemText(self.onglets.list_ver_installed.GetSelection()).encode('utf-8')
+	  def delete32(self, event):
+		version = self.onglets.list_ver_installed["x86"].GetItemText(self.onglets.list_ver_installed["x86"].GetSelection()).encode('utf-8')
 		if(wx.YES == wx.MessageBox("Are you sure you want to delete wine "+version+"?", style=wx.YES_NO | wx.ICON_QUESTION)):
-				shutil.rmtree(Variables.playonlinux_rep+"/WineVersions/"+version)
+				shutil.rmtree(Variables.playonlinux_rep+"/wine/"+os_pref+"-x86/"+version)
 
-		self.onglets.VersionsLoad()
-		self.add_games()
+	  def install32(self, event):
+		install = self.onglets.list_apps["x86"].GetItemText(self.onglets.list_apps["x86"].GetSelection()).encode('utf-8')
+		os.system("bash \""+Variables.playonlinux_env+"/bash/install_wver\" "+install+" x86 &")
 
-	  def install(self, event):
-		install = self.onglets.list_apps.GetItemText(self.onglets.list_apps.GetSelection()).encode('utf-8')
-		os.system("bash \""+Variables.playonlinux_env+"/bash/install_wver\" "+install+" &")
-		self.add_games()
 
-	  def getVersions(self):
-		self.download.thread_message = "get"
+	  def unselect64(self, event):
+		if(event.GetId() == 206):
+			self.onglets.list_ver_installed["amd64"].UnselectAll()
+			self.onglets.button_rm["amd64"].Enable(False)
+			self.onglets.button_in["amd64"].Enable(True)
+		if(event.GetId() == 207):
+			self.onglets.list_apps["amd64"].UnselectAll()		
+			self.onglets.button_rm["amd64"].Enable(True)
+			self.onglets.button_in["amd64"].Enable(False)
 
-	  def WriteVersions(self):
-		self.onglets.imagesapps.RemoveAll()
-		self.onglets.imagesapps_i.RemoveAll()
-		self.onglets.list_apps.DeleteAllItems()	
-		self.onglets.list_ver_installed.DeleteAllItems()	
+	  def delete64(self, event):
+		version = self.onglets.list_ver_installed["amd64"].GetItemText(self.onglets.list_ver_installed["amd64"].GetSelection()).encode('utf-8')
+		if(wx.YES == wx.MessageBox("Are you sure you want to delete wine "+version+"?", style=wx.YES_NO | wx.ICON_QUESTION)):
+				shutil.rmtree(Variables.playonlinux_rep+"/wine/"+os_pref+"-amd64/"+version)
 
-		root = self.onglets.list_apps.AddRoot("")
+	  def install64(self, event):
+		install = self.onglets.list_apps["amd64"].GetItemText(self.onglets.list_apps["amd64"].GetSelection()).encode('utf-8')
+		os.system("bash \""+Variables.playonlinux_env+"/bash/install_wver\" "+install+" amd64 &")
+		
+	  def getVersions(self, arch="x86"):
+		if(arch == "x86"):
+			self.download32.thread_message = "get"
+		if(arch == "amd64"):
+			self.download64.thread_message = "get"
+			
+
+	  def WriteVersion(self, arch="x86"):
+		self.onglets.imagesapps[arch].RemoveAll()
+		self.onglets.imagesapps_i[arch].RemoveAll()
+		self.onglets.list_apps[arch].DeleteAllItems()	
+		self.onglets.list_ver_installed[arch].DeleteAllItems()	
+
+		root = self.onglets.list_apps[arch].AddRoot("")
 		self.i = 0
-		while(self.i < len(self.download.versions)):
-			self.onglets.imagesapps.Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine-packages.png"))
-			self.onglets.list_apps.AppendItem(root,self.download.versions[self.i],self.i)
-			self.i += 1
+		if(arch == "x86"):
+			while(self.i < len(self.download32.versions)):
+				self.onglets.imagesapps[arch].Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine-packages.png"))
+				self.onglets.list_apps[arch].AppendItem(root,self.download32.versions[self.i],self.i)
+				self.i += 1
+		if(arch == "amd64"):
+			while(self.i < len(self.download64.versions)):
+				self.onglets.imagesapps[arch].Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine-packages.png"))
+				self.onglets.list_apps[arch].AppendItem(root,self.download64.versions[self.i],self.i)
+				self.i += 1
+				
+		root2 = self.onglets.list_ver_installed[arch].AddRoot("")
+		wfolder = os_pref+"-"+arch
 
-		root2 = self.onglets.list_ver_installed.AddRoot("")
-		installed_versions = os.listdir(Variables.playonlinux_rep+"/WineVersions/")
+		installed_versions = os.listdir(Variables.playonlinux_rep+"/wine/"+wfolder)
 		installed_versions.sort(key=keynat)
 		installed_versions.reverse()
 		self.i = 0
 		self.j = 0
 		while(self.i < len(installed_versions)):
-			if(os.path.isdir(Variables.playonlinux_rep+"/WineVersions/"+installed_versions[self.i])):
-				if(len(os.listdir(Variables.playonlinux_rep+"/WineVersions/"+installed_versions[self.i])) == 0):
-					self.onglets.imagesapps_i.Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine-warning.png"))
+			if(os.path.isdir(Variables.playonlinux_rep+"/wine/"+wfolder+"/"+installed_versions[self.i])):
+				if(len(os.listdir(Variables.playonlinux_rep+"/wine/"+wfolder+"/"+installed_versions[self.i])) == 0):
+					self.onglets.imagesapps_i[arch].Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine-warning.png"))
 				else:
-					self.onglets.imagesapps_i.Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine.png"))
-				self.onglets.list_ver_installed.AppendItem(root2,installed_versions[self.i].replace("wine-",""),self.j)
+					self.onglets.imagesapps_i[arch].Add(wx.Bitmap(Variables.playonlinux_env+"/etc/install/wine.png"))
+				self.onglets.list_ver_installed[arch].AppendItem(root2,installed_versions[self.i],self.j)
 				self.j += 1
 			self.i += 1
 		try :
 			if(versions[0] == "Wine packages website is unavailable"):
-				self.onglets.list_apps.Enable(False)
-				self.onglets.imagesapps.RemoveAll()
+				self.onglets.list_apps[arch].Enable(False)
+				self.onglets.imagesapps[arch].RemoveAll()
 		except :
 			pass
-		self.onglets.button_rm.Enable(False)
-		self.onglets.button_in.Enable(False)
+		self.onglets.button_rm[arch].Enable(False)
+		self.onglets.button_in[arch].Enable(False)
 
 	  def closeapp(self, event):
-		self.download.thread_running = False
+		self.download32.thread_running = False
+		self.download64.thread_running = False
+
 		self.Destroy()   
 
 
