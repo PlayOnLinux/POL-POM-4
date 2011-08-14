3db45109fb98d38b7c6a6a76b6bd3087e62f04d0
Quentin Pâris <github@playonlinux.com>
2011-08-14 13:29:46 +0200

    wine link fixed

--- python/lib/Variables.py
+++ python/lib/Variables.py
@@ -39,7 +39,7 @@ os.environ["SITE"] = "http://repository.playonlinux.com"
 os.environ["VERSION"] = "4.0-dev"
 os.environ["POL_ID"] = str(random.randint(1,100000000))
 os.environ["WINEPREFIX"] = os.environ["REPERTOIRE"]+"/wineprefix/default"
-os.environ["WINE_SITE"] = "http://ks383805.kimsufi.com/binaries/" # Temporary 
+os.environ["WINE_SITE"] = "http://ks383805.kimsufi.com/wine/binaries/" # Temporary 
 homedir = os.environ["HOME"]
 
 # Si DYLD_LIBRARY_PATH n'existe pas, on la defini pour etre sur	
