b7802c237880dc9347ad1441512eb74c2fcdc03b
Quentin Pâris <github@playonlinux.com>
2011-09-18 22:39:45 +0200

    Cleaning...

--- python/lib/lng.py
+++ python/lib/lng.py
@@ -8,20 +8,6 @@ import locale, string
 
 class Lang(object):
 	def __init__(self):
-		"""
-		languages = os.listdir(Variables.playonlinux_env+'/lang/locale')
-
-		langid = wx.LANGUAGE_DEFAULT
-		basepath = os.popen("printf \"$PLAYONLINUX\"","r").read()
-		localedir = os.path.join(basepath, "lang/locale")
-		domain = "pol"
-		mylocale = wx.Locale(langid)
-		mylocale.AddCatalogLookupPathPrefix(localedir)
-		mylocale.AddCatalog(domain)
-
-		#mytranslation = gettext.translation(domain, localedir, [mylocale.GetCanonicalName()], fallback = True)
-		#mytranslation.install()
-		"""
 		return None
 
 class iLang(object):
