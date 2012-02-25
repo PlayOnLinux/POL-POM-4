f486d79f7d97a507906348243a1664b3eabff8b4
Quentin Pâris <git@qparis.fr>
2012-02-25 18:02:05 +0100

    Removing glut depends

--- src/amd64.check.c
+++ src/amd64.check.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <GL/glut.h> 
 
-// Built with gcc -o amd64.check  -lglut -lGL -lGLU -lX11 -lXmu -lXi -lm
+// Built with gcc -o amd64.check  -lGL -lGLU -lX11 -lXmu -lXi -lm
 int main(void)
 {
 	printf("OpenGL 64bits is working\n");
--- src/x86.check.c
+++ src/x86.check.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <GL/glut.h> 
 
-// Built with gcc -o x86.check -m32 -lglut -lGL -lGLU -lX11 -lXmu -lXi -lm
+// Built with gcc -o x86.check -m32 -lGL -lGLU -lX11 -lXmu -lXi -lm
 int main(void)
 {
 	printf("OpenGL 32bits is working\n");
