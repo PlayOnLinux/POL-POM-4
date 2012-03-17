bc856caeb66afdf48d4eda8ef5507e5f0615f3eb
Quentin Pâris <git@qparis.fr>
2012-03-17 19:55:14 +0100

    Remove useless files

--- bash/test.py
+++
@@ -1,6 +0,0 @@
-from subprocess import Popen, PIPE, STDOUT
-
-cmd = 'wineconsole 2>&1'
-p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-output = p.stdout.read()
-print output
