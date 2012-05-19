da78f4464c34ce9890487bd832353428f04dccf0
Quentin Pâris <git@qparis.fr>
2012-05-19 15:38:00 +0200

    4.1.0

--- python/lib/Variables.py
+++ python/lib/Variables.py
@@ -51,7 +51,7 @@ else:
 os.environ["POL_USER_ROOT"] = os.environ["REPERTOIRE"]
 os.environ["TITRE"] = os.environ["APPLICATION_TITLE"]
 os.environ["SITE"] = "http://repository.playonlinux.com"
-os.environ["VERSION"] = "4.1-dev"
+os.environ["VERSION"] = "4.1.0-dev"
 os.environ["POL_ID"] = str(random.randint(1,100000000))
 os.environ["WINEPREFIX"] = os.environ["REPERTOIRE"]+"/wineprefix/default"
 os.environ["WINE_SITE"] = "http://www.playonlinux.com/wine/binaries"
