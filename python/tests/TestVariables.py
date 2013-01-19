36689707bd30489f1c84e6487a790f979a56fa64
Pierre Etchemaite <petchema@concept-micro.com>
2013-01-19 12:30:43 +0100

    TestVariables: fix some tests
    The reload of Variables does not seem to work between architecture test
    suites :(

--- python/tests/TestVariables.py
+++ python/tests/TestVariables.py
@@ -6,12 +6,8 @@ class TestVariables(unittest.TestCase):
     def setUp(self):
         self.environ_backup = os.environ
 
-    def _init(self, operating_system):
-        if operating_system:
-            os.environ["POL_OS"] = operating_system
-        elif "POL_OS" in os.environ:
-            del(os.environ["POL_OS"])
-        import Variables
+    def _init(self):
+        import lib.Variables as Variables
 
     def tearDown(self):
         if 'Variables' in sys.modules:
@@ -21,7 +17,9 @@ class TestVariables(unittest.TestCase):
 
 class TestVariablesNoOS(TestVariables):
     def _init(self):
-        TestVariables._init(self, None)
+        if "POL_OS" in os.environ:
+            del(os.environ["POL_OS"])
+        TestVariables._init(self)
     
     def test_checks_pol_os_present(self):
         self.assertRaises(SystemExit, self._init)
@@ -43,13 +41,13 @@ class TestVariablesSupportedOS(TestVariables):
         self.assertIn('REPERTOIRE', os.environ)
         self.assertIn('POL_USER_ROOT', os.environ)
         self.assertIn('APPLICATION_TITLE', os.environ)
-        self.assertIn('TITLE', os.environ)
+        self.assertIn('TITRE', os.environ)
         self.assertIn('POL_DNS', os.environ)
         self.assertIn('POL_WGET', os.environ)
         self.assertIn('WINEPREFIX', os.environ)
         self.assertIn('WINEDLLOVERRIDES', os.environ)
         self.assertIn('AMD64_COMPATIBLE', os.environ)
-        self.assertIn(os.environ['AMS64_COMPATIBLE'], ['True', 'False'])
+        self.assertIn(os.environ['AMD64_COMPATIBLE'], ['True', 'False'])
         self.assertIn('DYLD_LIBRARY_PATH', os.environ)
         self.assertIn('LD_LIBRARY_PATH', os.environ)
         self.assertIn('PATH_ORIGIN', os.environ)
@@ -60,21 +58,25 @@ class TestVariablesSupportedOS(TestVariables):
 
 class TestVariablesLinux(TestVariablesSupportedOS):
     def _init(self):
-        TestVariables._init(self, "Linux")
+        os.environ["POL_OS"] = "Linux"
+        os.environ["MACHTYPE"] = "x86_64"
+        TestVariables._init(self)
 
     def test_linux_environment(self):
         self._init()
-        self.assertEqual(on.environ["POL_OS"], "Linux")
+        self.assertEqual(os.environ["POL_OS"], "Linux")
         self._common_variables_present()
 
 
 class TestVariablesOSX(TestVariablesSupportedOS):
     def _init(self):
-        TestVariables._init(self, "Mac")
+        os.environ["POL_OS"] = "Mac"
+        os.environ["MACHTYPE"] = "x86_64-apple-darwin12"
+        TestVariables._init(self)
 
     def test_osx_environment(self):
         self._init()
-        self.assertEqual(on.environ["POL_OS"], "Mac")
+        self.assertEqual(os.environ["POL_OS"], "Mac")
         self._common_variables_present()
         self.assertIn('PLAYONMAC', os.environ)
         self.assertIn('MAGICK_HOME', os.environ)
